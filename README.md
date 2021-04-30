463c54f0ee3670b1f1f3c4213b6a77aeeef12e46
GingkathFox <gingkathfox@gmail.com>
2021-04-30 00:03:29 -0400

    Cleanup

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 Assuming you have [Node.JS and NPM](https://nodejs.org/en/download/) installed ([Linux installation](https://fossbytes.com/how-to-install-nodejs-on-linu/)), run `npm i -g svmodhandler` to install.
 
-Or, if you wish to do it the hard way, run `npm i` to get the dependencies and `npm link` to link to your `NODE_PREFIX`.
+Or, if you wish to do it the hard way, `git clone` the repo, run `npm i` to get the dependencies, and `npm link` to link to your `NODE_PREFIX`.
 
 # Usage
 
--- src/handler.js
+++ src/handler.js
@@ -9,7 +9,7 @@ const CWD = process.cwd()
 
 async function handle() {
     // get all files and folders, then filter out the files
-    const contents = await fs.readdirSync(path.normalize(CWD), {
+    const contents = fs.readdirSync(path.normalize(CWD), {
         encoding: 'utf8',
         withFileTypes: true
     })
@@ -25,17 +25,18 @@ async function handle() {
 
     // create the choices and flatten the array
     const choices = [
-        new inquirer.Separator(' = Enabled = '),
+        new inquirer.Separator(chalk.greenBright(' = Enabled = ')),
         enabledMods,
-        new inquirer.Separator(' = Disabled = '),
+        new inquirer.Separator(chalk.redBright(' = Disabled = ')),
         disabledMods
     ].flat(2)
 
     // now create the question
     const modQuestion = {
         type: 'checkbox',
+        prefix: '',
         message: `Tick each box to enable or disable a mod!`,
-        name: "mods",
+        name: "results",
         choices: choices,
         validate: (answer) => {
             if (answer.length < 1) {
@@ -44,34 +45,31 @@ async function handle() {
             return true
         }
     }
-    const results = await inquirer.prompt(modQuestion)
+    const {
+        results
+    } = await inquirer.prompt(modQuestion)
 
     // now enable and disable the mods
-    for (mod of results.mods) {
-        // get the dir object
-        const dir = dirs.find(v => v.name === mod)
-        // check if its disabled...
-        if (dir.name.startsWith(`.`)) {
-            // ...if so...
-            const noDotName = dir.name.slice(1) // ...remove the `.`...
-            try {
+    for (mod of results) {
+        try {
+            // get the dir object
+            const dir = dirs.find(v => v.name === mod)
+            // check if its disabled...
+            if (dir.name.startsWith(`.`)) {
+                // ...if so...
+                const noDotName = dir.name.slice(1) // ...remove the `.`...
                 // ...rename the folder...
-                await fs.renameSync(path.normalize(path.join(CWD, dir.name)), path.normalize(path.join(CWD, noDotName)))
+                fs.renameSync(path.normalize(path.join(CWD, dir.name)), path.normalize(path.join(CWD, noDotName)))
                 // ...and alert the user
                 console.log(chalk.greenBright(`The mod "${chalk.yellowBright(noDotName)}" was enabled!`))
-            } catch (e) {
-                console.log(chalk.red.bold(`There was an error! Exiting...\nError: ${e.stack}`))
-                process.exit(1)
-            }
-        } else {
-            // and do the same down here, except this time we're adding the `.` to disable it
-            try {
-                await fs.renameSync(path.normalize(path.join(CWD, dir.name)), path.normalize(path.join(CWD, `.${dir.name}`)))
+            } else {
+                // and do the same down here, except this time we're adding the `.` to disable it
+                fs.renameSync(path.normalize(path.join(CWD, dir.name)), path.normalize(path.join(CWD, `.${dir.name}`)))
                 console.log(chalk.greenBright(`The mod "${chalk.yellowBright(dir.name)}" was disabled!`))
-            } catch (e) {
-                console.log(chalk.red.bold(`There was an error! Exiting...\nError: ${e.stack}`))
-                process.exit(1)
             }
+        } catch (e) {
+            console.log(chalk.red.bold(`There was an error! Exiting...\nError: ${e.stack}`))
+            process.exit(1)
         }
     }
 }
